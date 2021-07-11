52b8901ef6e525a3c2ce1aa2860ed5a806032e3b
maricotagc <maricotagc@gmail.com>
2021-07-11 13:40:04 +0200

    version # 6 of exercise 4

--- src/main/java/mari/moita/com/Exercise4.java
+++ src/main/java/mari/moita/com/Exercise4.java
@@ -4,8 +4,8 @@ import java.util.List;
 
 public class Exercise4 {
 
-    static final String MALE = null;
-    static final String FEMALE = null;
+    static final String MALE = "male";
+    static final String FEMALE = "female";
 
     static final int MALE_INDEX = 0;
     static final int FEMALE_INDEX = 1;
@@ -34,11 +34,11 @@ public class Exercise4 {
         Person[] agePeople = new Person[2];
 
         for (Person person : people) {
-            if ("male".equals(person.getGender())) {
+            if (MALE.equals(person.getGender())) {
                 if (agePeople[MALE_INDEX] == null || person.getAge() > agePeople[0].getAge()) {
                     agePeople[MALE_INDEX] = person;
                 }
-            } else if ("female".equals(person.getGender())) {
+            } else if (FEMALE.equals(person.getGender())) {
                 if (agePeople[FEMALE_INDEX] == null || person.getAge() < agePeople[1].getAge()) {
                     agePeople[FEMALE_INDEX] = person;
                 }
